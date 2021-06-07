05aaf4fb99d2036b87d1cecebab6a557f120feda
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-06-07 12:38:23 +0530

    added: html elements
    basic playing with js functions and callbacks

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("banana")
+const btnTransalate=document.querySelector("#btn-translate");
+const inputText=document.querySelector("#input");
+const output=document.querySelector("#output");
+
+btnTransalate.addEventListener("click",()=>
+{
+    var empty=checkIfEmpty();
+    if(empty==false)
+    {
+        translate();
+        alert("translated")
+    }
+    else
+    {
+        inputText.focus();
+        translate();
+    }
+    
+})
+
+
+
+function checkIfEmpty()
+{
+    if(inputText.value=="")
+    {
+        alert("Enter some text");
+        return true;
+    }
+}
+
+function translate()
+{
+    output.innerHTML=inputText.value;
+}
