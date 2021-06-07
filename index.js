4f5c86d7367a0862e32f82bacbb500b00e737a90
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-06-07 14:16:50 +0530

    update: fetch function

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const btnTransalate=document.querySelector("#btn-translate");
 const inputText=document.querySelector("#input");
 const output=document.querySelector("#output");
 
-btnTransalate.addEventListener("click",()=>
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var data="sushant";
+
+
+function clickHandler()
 {
-    var empty=checkIfEmpty();
-    if(empty==false)
-    {
-        translate();
-        alert("translated")
-    }
-    else
-    {
-        inputText.focus();
-        translate();
-    }
-    
-})
+    checkIfEmpty();
+    var url2=url+"?text="+inputText.value;
+    fetch(url2)
+    .then(response=>response.json())
+    .then(json=>console.log(json))
+    //catch(errorHandler)
+}
+
+// function errorHandler(error)
+// {
+//     console.log("error occured");
+//     alert("something went wrong")
+// }
+
+btnTransalate.addEventListener("click",clickHandler());
 
 
 
